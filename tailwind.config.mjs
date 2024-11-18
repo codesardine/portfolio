41b51b1e8a75f7f76bec81f72a2e6e2d940f06a5
vlopes <vmnlop@gmail.com>
2024-11-18 01:29:56 +0000

    [blog] fix prose colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,36 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			dropShadow: {
+				'yellow': '0 35px 35px #ffee58',
+			  },
+			typography: ({ theme }) => ({
+				vit: {
+				  css: {
+					'--tw-prose-body': theme('colors.gray[800]'),
+					'--tw-prose-headings': theme('colors.gray[700]'),
+				  },
+				},
+			  }),
+		},
 	},
 	plugins: [
 		require('daisyui'),
+		require('@tailwindcss/typography'),
 	  ],
+	  daisyui: {
+		themes: ["light"], 
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: "",
+	  },	
+	  future: {
+		hoverOnlyWhenSupported: true,
+	  },
+	  colorMode: {
+		disableModernColorFormat: true
+	  }
 }
